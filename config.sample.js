f92c1b2f098e6484b8f1d8f00f45538418bc5f70
Harest <Harest@users.noreply.github.com>
2019-07-19 23:16:10 +0200

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -3,8 +3,8 @@ module.exports = {
   consumer_secret: '##',
   access_token_key: '##',
   access_token_secret: '##',
-  path: './tweets.csv', // path to the archive .csv file
-  maxDate: false, // delete older than this eg: '2011-12-31 00:00:00 +0000'
+  path: './tweet.js', // path to the archive .js file
+  maxDate: false, // delete older than this eg: '2011-12-31T00:00:00+0000'
   saveRegexp: [], // save tweets matching this regexp: eg ['#newavatar', '@\w+']
   callsInterval: 500 // ms
 }
